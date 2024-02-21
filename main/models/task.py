a5c8a4bc4290c2356d89a16af9cfc0d8bc3fe1e1
sawyre <sawyre@mail.ru>
2024-02-22 01:37:45 +0300

    feat: Add task and tag models. Add name and email fields in User. Add migrations

---
+++ main/models/task.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime
+from .user import User
+from .tag import Tag
+
+
+class Task(models.Model):
+    class Statuses(models.TextChoices):
+        NEW_TASK = "new_task"
+        IN_DEVELOPMENT = "in_development"
+        IN_QA = "in_qa"
+        IN_CODE_REVIEW = "in_code_review"
+        READY_FOR_RELEASE = "ready_for_release"
+        REALESED = "released"
+        ARCHIVED = "archived"
+        
+    title = models.CharField(max_length=50)
+    description = models.TextField(default="", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(null=True)
+    status = models.DateField(default=Statuses.NEW_TASK, choices=Statuses.choices)
+    priority = models.PositiveIntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    executor = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="executor")
+    tags = models.ManyToManyField(Tag)
+    
+    class Meta:
+        ordering = ["-priority"]
--- main/models/user.py
+++ main/models/user.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import EmailValidator
 from django.db import models
 
 
@@ -8,6 +9,9 @@ class User(AbstractUser):
         MANAGER = "manager"
         ADMIN = "admin"
 
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     role = models.CharField(
         max_length=255, default=Roles.DEVELOPER, choices=Roles.choices
     )
+    mail = models.CharField(null=True, validators=[EmailValidator])
