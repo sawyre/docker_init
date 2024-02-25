7240f18a21b55e16d4096bff2dfdef84074f4259
Vladislav Vlasenko <vvlasenko@AD.SPARM.COM>
2024-02-25 07:00:12 +0300

    fix: Change field type for status of task

--- main/models/task.py
+++ main/models/task.py
@@ -21,7 +21,7 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deadline = models.DateTimeField(null=True)
-    status = models.DateField(default=Statuses.NEW_TASK, choices=Statuses.choices)
+    status = models.CharField(default=Statuses.NEW_TASK, choices=Statuses.choices)
     priority = models.PositiveIntegerField(
         default=1, validators=[MaxValueValidator(10), MinValueValidator(1)]
     )
@@ -35,18 +35,29 @@ class Task(models.Model):
 
     class Meta:
         ordering = ["-priority"]
-    
+
     def change_status(self, new_status: Statuses) -> None:
         available_status_changes = {
-            self.Statuses.NEW_TASK: [self.Statuses.IN_DEVELOPMENT, self.Statuses.ARCHIVED],
+            self.Statuses.NEW_TASK: [
+                self.Statuses.IN_DEVELOPMENT,
+                self.Statuses.ARCHIVED,
+            ],
             self.Statuses.IN_DEVELOPMENT: [self.Statuses.IN_QA],
-            self.Statuses.IN_QA: [self.Statuses.IN_DEVELOPMENT, self.Statuses.IN_CODE_REVIEW],
-            self.Statuses.IN_CODE_REVIEW: [self.Statuses.READY_FOR_RELEASE, self.Statuses.IN_DEVELOPMENT],
+            self.Statuses.IN_QA: [
+                self.Statuses.IN_DEVELOPMENT,
+                self.Statuses.IN_CODE_REVIEW,
+            ],
+            self.Statuses.IN_CODE_REVIEW: [
+                self.Statuses.READY_FOR_RELEASE,
+                self.Statuses.IN_DEVELOPMENT,
+            ],
             self.Statuses.READY_FOR_RELEASE: [self.Statuses.REALESED],
-            self.Statuses.REALESED: [self.Statuses.ARCHIVED]
+            self.Statuses.REALESED: [self.Statuses.ARCHIVED],
         }
-        
-        if self.status in available_status_changes and \
-           new_status in available_status_changes[self.status]:
+
+        if (
+            self.status in available_status_changes
+            and new_status in available_status_changes[self.status]
+        ):
             self.status = new_status
             self.save()
