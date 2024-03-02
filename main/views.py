ad961363529f2f662af15eb4e9a3d35f820ba892
sawyre <sawure@mail.com>
2024-03-02 16:11:51 +0300

    feat: add view sets

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from serializers import UserSerializer, TaskSerializer, TagSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.order_by("id")
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = TaskSerializer.objects.select_related(
+        "asignee", "created_by"
+    ).prefetch_related("tags")
+    serializer_class = TaskSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = TagSerializer.objects.order_by("id")
+    serializer_class = TagSerializer
