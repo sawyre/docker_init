dc2ebba552ba92abd1170d7cb587952b78068673
vladislav.vlasenko <vladislav.vlasenko@sparm.com>
2024-03-11 15:10:25 +0300

    feat: Add filtersets for Tasks and Users

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -55,6 +55,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "task_manager.urls"
 
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
