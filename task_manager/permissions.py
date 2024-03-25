df9f65d6b1f890ba3011bc43d7640d9960676c20
sawyre <sawure@mail.com>
2024-03-25 14:29:25 +0300

    fix: Fix model permissions

--- task_manager/permissions.py
+++ task_manager/permissions.py
@@ -1,33 +1,9 @@
 from rest_framework import permissions
 
 
-class AdminModelPermissions(permissions.DjangoModelPermissions):
-    perms_map = {
-        'GET': ['%(app_label)s.view_%(model_name)s'],
-        'OPTIONS': [],
-        'HEAD': ['%(app_label)s.view_%(model_name)s'],
-        'POST': ['%(app_label)s.add_%(model_name)s'],
-        'PUT': ['%(app_label)s.change_%(model_name)s'],
-        'PATCH': ['%(app_label)s.change_%(model_name)s'],
-    }
-
+class AdminModelPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        if not request.user or (
-           not request.user.is_authenticated and self.authenticated_users_only):
+        if not request.user.is_staff and (request.method == "DELETE"):
             return False
 
-        if not request.user.is_staff and not (request.method in self.perms_map):
-            return False
-
-        if getattr(view, '_ignore_model_permissions', False):
-            return True
-
-        queryset = self._queryset(view)
-        perms = self.get_required_permissions(request.method, queryset.model)
-        change_perm = self.get_required_permissions('PUT', queryset.model)
-
-        user = request.user
-        if request.method == 'GET':
-            return user.has_perms(perms) or user.has_perms(change_perm)
-
-        return user.has_perms(perms)
+        return True
